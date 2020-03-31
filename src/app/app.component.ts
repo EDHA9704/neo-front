6ca466ab282050c290d3bcfb64298321ce944f7c
Dave hernandez <david-hernadezbgu@gmail.com>
2020-03-31 01:00:45 -0500

    ass

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
+import { AuthenticationService } from './_shared/services';
+import { User, Role } from './_models';
+import $ from "jquery";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +11,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'neo-front';
+  currentUser: User; 
+  constructor(
+    private router: Router,
+    private authenticationService: AuthenticationService
+) {
+    this.authenticationService.currentUser.subscribe(x => {
+      console.log(x)
+      this.currentUser = x
+    }
+     );
+}
+get isAdmin() {
+  return this.currentUser && this.currentUser.rol === Role.Admin;
+}
+ngOnInit() {
+$( document ).ready(()=> {
+  console
+  .log("viendo scroll")
+  $(window).scroll(function() {
+   console.log('OK FUNCIONA')
+  });
+});
+console.log("ENTRO APP")
+
+var OneSignal = window['OneSignal'] || [];
+OneSignal.push(function() { 
+  OneSignal.init({
+    appId: "039f7bfd-cb61-429d-a8b7-5b5cfd4033a8",
+  });
+}); 
+
+
+}
+logout() {
+  this.authenticationService.logout();
+  this.router.navigate(['/login']);
+}
+
 }
